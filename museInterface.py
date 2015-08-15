75cc160b21720a0d834e9952ba8cc002dfeb4b70
John Fish <john@johnafish.ca>
2015-08-15 19:26:52 -0400

    Deleted a bunch of unnecessary files.

--- museInterface.py
+++ museInterface.py
@@ -48,7 +48,7 @@ def endServer():
 screen = Canvas(master, width=APP_WIDTH, height=APP_HEIGHT, background='#0047B2')
 screen.pack()
 
-screen.create_text(APP_WIDTH/2, APP_HEIGHT/4, text="museTation", font=("Arial", 50))
+screen.create_text(APP_WIDTH/2, APP_HEIGHT/4, text="museTation", font=("Arial", 50), fill="#FFFFFF")
 
 start = Button(screen, text='Start', width=25, height=2, bd=2, font=("Sans Serif", 15), activebackground='#4D94FF', background='#4D94FF', command=startServer)
 start.place(x=APP_WIDTH/2, y=APP_HEIGHT/8*5, anchor='center')
